279b99716240cf3aaa5a0d9f4fe7fabe791a178f
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-05 08:37:39 -0300

    Atualiza condicional composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -17,14 +17,36 @@ if($numero > 1){
     echo "<p>$numero é maior que 1</p>";
 }
 
+
+
 // Sintaxe omitindo as chaves
 if($numero > 1) echo "<p>$numero é maior que 1</p>";
 ?>
 
 
     <h2>Composta</h2>
+ <?php 
+ $produto = "ultraboock Azus";
+ $qtEstoque = 10;//o que temos
+ $qtCritica = 5;//minimo necessario
 
+echo "<h3>$produto</h3>";
+echo "<h4>Quantidade em estoque: $qtEstoque</h4>";
+
+if ($qtEstoque < $qtCritica) {
+    echo  "<p>É necessario comprar!</p>";
+
+} else {
+    echo "<p>estoque normal</p>";
+}
+
+ ?>   
+ 
     <h2>Encadeada</h2>
+    <hr>
+
+    
+
 
 </body>
 </html>
