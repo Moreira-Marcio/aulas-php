40ba6cf89322b25677fe9648ad535d041f86afd7
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-06 12:01:08 -0300

    exercicio loop estrutura de dados

---
+++ exercicios/exerccio-04.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 04</title>
+    <style>
+        .table {
+            width: 60%;
+            border-collapse: collapse;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 10px;
+            text-align: center;
+        }
+        th {
+            background-color: #007BFF;
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+      
+    </style>
+</head>
+<body>
+    <h1>Loop e Estrutura de Dados</h1>
+    <p>Crie uma estrutura de dados (pode ser um array indexado, ou array associativo, ou array de objetos, ou matriz) com 6 nomes de linguagens e uma pequena descrição para cada.</p>
+<table class="table">
+    <tr>
+    <th><b>Linguagem</b></th>
+    <th><b>Caracteristica</b></th>
+    </tr>
+    
+    
+ <?php
+ $linguagens = [
+    "HTML" => "Estruturação",    
+     "CSS" => "Estilos",
+      "JS" => "Comportamentos",
+      "PHP" => "Back-End",
+      "SQL" => "Manipulação de dados",
+      "Java" => "Softwares"
+ ];
+
+ foreach ($linguagens as $linguagem =>$caracteristica) {
+ ?>
+    <tr>
+        <td><?=$linguagem?></td>
+        <td><?=$caracteristica?></td>
+    </tr> 
+ <?php
+ }
+ ?>  
+ </table>   
+</body>
+</html>
