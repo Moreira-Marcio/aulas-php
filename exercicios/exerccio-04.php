cf5bb9b77724b8b3e3138ca667022049217b86e1
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-10 11:28:18 -0300

    atualiza tabela com bootstrap

--- exercicios/exerccio-04.php
+++ exercicios/exerccio-04.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 04</title>
-    <style>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+     <!-- <style>
         .table {
             width: 60%;
             border-collapse: collapse;
@@ -28,20 +30,22 @@
         tr:nth-child(even) {
             background-color: #f2f2f2;
         }
-    </style>
+    </style> -->
 </head>
 
 <body>
+    <div class="text-center">
     <h1>Loop e Estrutura de Dados</h1>
     <p>Crie uma estrutura de dados (pode ser um array indexado, ou array associativo, ou array de objetos, ou matriz) com 6 nomes de linguagens e uma pequena descrição para cada.</p>
-    <table class="table">
+    <table class="table table-hover table-secondary">
+        <thead class="table-dark">
         <tr>
-            <th><b>ID</b></th>
+            <th ><b>ID</b></th>
             <th><b>Linguagem</b></th>
             <th><b>Caracteristica</b></th>
         </tr>
-
-
+       </thead>
+       <tbody>
         <?php
         $i = 1;
 
@@ -59,13 +63,18 @@
         ?>
             <tr>
                 <td><?= $i++ ?></td>
-                <td><?= $linguagem ?></td>
+                <td class="table-primary"><?= $linguagem ?></td>
                 <td><?= $caracteristica ?></td>
             </tr>
         <?php
         }
         ?>
+        </tbody>
+
     </table>
+   </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 
 </html>
