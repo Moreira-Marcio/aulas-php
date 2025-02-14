1a857e2b8a49a599d43dad027cc523a953a3c447
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-14 08:29:22 -0300

    cria index basica

---
+++ 11-site/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Duvidas</a>
+            </nav>
+        </header>
+        <main>
+            <h2>Bem vindo ao siyte exemplo</h2>
+            <p>Esta  é a primeira pagina do nosso site.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium expedita numquam laboriosam. Voluptate distinctio, illo dolor nobis, eum aperiam, non laudantium mollitia cum ea dolore facere delectus expedita aspernatur aut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam commodi, sed optio veniam aspernatur voluptas perspiciatis facilis quibusdam, nostrum vero cum, error modi! Vitae tempora provident quisquam ullam esse harum?</p>
+        </main>
+        <footer>
+            <p>Site criado para o curso Tec. de Informatica para Internet</p>
+        </footer>
+        <hr>
+    </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>    
+</body>
+</html>
