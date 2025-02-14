a6c434005225417d835cbc8175f16f0bb6b5fd41
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-14 11:46:36 -0300

    adiciona pagina de formulario

--- 11-site/includes/rodape.php
+++ 11-site/includes/rodape.php
@@ -3,11 +3,24 @@
         <footer>
             <p>Site criado para o curso Tec. de Informatica para Internet</p>
             <?php
-               date_default_timezone_set('America/Sao_Paulo');
-               $dataHora = new DateTime();
-               echo $dataHora->format('d/m/Y H:i:s');
-                
+              // date_default_timezone_set('America/Sao_Paulo');
+              // $dataHora = new DateTime();
+             //  echo $dataHora->format('d/m/Y H:i:s');
+             
+             // Definir o locale como 'pt_BR' (português do Brasil)
+             setlocale(LC_TIME, 'pt_BR.UTF-8');
+             
+             // Criar um objeto DateTime
+             $data = new DateTime();
+             
+             // Usar IntlDateFormatter para formatar a data por extenso
+             $fmt = new IntlDateFormatter('pt_BR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+             $data_extenso = $fmt->format($data);
+             
+             
+             
             ?>
+            <p><?=$data_extenso?></p>
         </footer>
         <hr>
     </div>
