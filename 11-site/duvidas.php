472cf4bfd0b0d5255b1c1fda71b340aa791c6a92
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-14 08:53:24 -0300

    adiciona fragmento cabecalho

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Duvidas site com PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
-</head>
-<body>
-    <div class="container">
-        <header>
-            <h1>Site com PHP</h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="cursos.php">Cursos</a>
-                <a href="duvidas.php">Duvidas</a>
-            </nav>
-        </header>
-        <main>
+<?php include "cabecalho.php" ?>
             <h2>Pagina duvidas</h2>
             
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatum nemo, minus voluptatibus natus, officiis amet explicabo, doloremque voluptates ex aut. Ex cum voluptatibus unde ut aut sint magni veritatis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis officia quia, commodi sapiente at voluptatibus, et, fuga officiis est assumenda nesciunt quibusdam placeat suscipit! Dicta ea nam maiores. Commodi, enim!</p>
--- 11-site/index.php
+++ 11-site/index.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
-</head>
-<body>
-    <div class="container">
-        <header>
-            <h1>Site com PHP</h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="cursos.php">Cursos</a>
-                <a href="duvidas.php">Duvidas</a>
-            </nav>
-        </header>
-        <main>
-            <h2>Bem vindo ao siyte exemplo</h2>
+<?php include "cabecalho.php" ?>
+            <h2>Bem vindo ao site exemplo</h2>
             <p>Esta  é a primeira pagina do nosso site.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium expedita numquam laboriosam. Voluptate distinctio, illo dolor nobis, eum aperiam, non laudantium mollitia cum ea dolore facere delectus expedita aspernatur aut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam commodi, sed optio veniam aspernatur voluptas perspiciatis facilis quibusdam, nostrum vero cum, error modi! Vitae tempora provident quisquam ullam esse harum?</p>
         </main>
