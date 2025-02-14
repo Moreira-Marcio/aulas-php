ac9c6c7f2fbaeb69ebd49a9437390f6183e7914d
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-14 08:38:22 -0300

    cria pagina duvidas com PHP

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cursos site com PHP</title>
+    <title>Duvidas site com PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
 </head>
@@ -18,14 +18,20 @@
             </nav>
         </header>
         <main>
-            <h2>Pagina de cursos</h2>
-            <p>Veja os cursos disponiveis</p>
-            <ul>
-                <li>Culinaria</li>
-                <li>Direção Defensiva</li>
-                <li>Como escapar de um terremoto</li>
-            </ul>
+            <h2>Pagina duvidas</h2>
             
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatum nemo, minus voluptatibus natus, officiis amet explicabo, doloremque voluptates ex aut. Ex cum voluptatibus unde ut aut sint magni veritatis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis officia quia, commodi sapiente at voluptatibus, et, fuga officiis est assumenda nesciunt quibusdam placeat suscipit! Dicta ea nam maiores. Commodi, enim!</p>
+
+            <h3>Principais duvidas</h3>
+            <details>
+                <summary>Como se cadastrar</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt reprehenderit aperiam facere? At consequuntur earum nulla amet sapiente exercitationem blanditiis nobis. Recusandae nulla minima repellat odio reprehenderit repudiandae voluptatum officia?</p>
+            </details>
+            <details>
+                <summary>Os cursos são gratuitos</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, nobis? Magni porro quia ea voluptas! Asperiores accusantium magni, quibusdam praesentium non incidunt ipsum modi ducimus rem perspiciatis architecto, beatae molestiae!</p>
+            </details>
+           
         </main>
         <footer>
             <p>Site criado para o curso Tec. de Informatica para Internet</p>
