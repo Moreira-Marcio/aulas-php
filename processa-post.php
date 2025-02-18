6661a623eea6c0c135e6912048d26d24f94b7b87
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-02-18 11:01:33 -0300

    corrige exercicio validação

--- processa-post.php
+++ processa-post.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,54 +8,64 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-<div class="container">
-    <h1>Processamento usando POST</h1>
-    <hr>
-<?php
-// Capturando os dados transmitidos
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$idade = $_POST["idade"];
-$mensagem = $_POST["mensagem"];
+    <div class="container">
+        <h1>Processamento usando POST</h1>
+        <hr>
+        <?php
+        if (empty($_POST["nome"]) || empty($_POST["email"])) { ?>
+            <p class="alert alert-danger">Por favor preencha o campo nome e email</p>
+        <?php
+        } else {
+            // Capturando os dados transmitidos
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $idade = $_POST["idade"];
+            $mensagem = $_POST["mensagem"];
 
-//capturando os options
-//solução 1: aplicar um ternario checando se existe algum interesse
-//$interesses = isset($_POST["interesses"]) ? $_POST["interesses"]: [];
-//solução 2: usando o operador de coalescencia nula  ??
-$interesses = $_POST["interesses"] ?? [];
+            //capturando os options
+            //solução 1: aplicar um ternario checando se existe algum interesse
+            //$interesses = isset($_POST["interesses"]) ? $_POST["interesses"]: [];
+            //solução 2: usando o operador de coalescencia nula  ??
+            $interesses = $_POST["interesses"] ?? [];
 
-$informativos = $_POST["informativos"];
+            $informativos = $_POST["informativos"] ?? [];
 
-?>
-    <!-- Exibindo -->
-    <h2>Dados:</h2>
-    <ul>
-        <li>Nome: <?=$nome?></li>
-        <li>E-mail: <?=$email?></li>
-        <li>Idade: <?=$idade?> anos</li>
-        <li>receber informativos: <?=$informativos?></li>
-    <!--usamos o empty com versão logica (operador! de negação ) portanto, se não esta vazio, mostre os interesses -->
-    <?php if(!empty($interesses)) {?>
-        <li>Interesses usando <code>implode()</code>:
-            <!--transformando o array em string-->
-             <?=implode(",",$interesses)?>
-        </li>
-        <li>interesses - usando <code>foreach</code>:
+        ?>
+            <!-- Exibindo -->
+            <h2>Dados:</h2>
             <ul>
-                <?php foreach($interesses as $interesse) {?>
-                    <li><?=$interesse?></li>
-                <?php }?>
-           
-            </ul>
-     
-        </li>
-     <?php }?>
-        <li>Mensagem: <?=$mensagem?></li>
-    </ul>
-        
-</div>    
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                <li>Nome: <?= $nome ?></li>
+                <li>E-mail: <?= $email ?></li>
+
+                <li>Idade: <?= $idade ?> anos</li>
+                <?php if (!empty ($informativos)){?>
+                    <li>receber informativos: <?= $informativos ?></li>
+                <?php } ?>
+                <!--usamos o empty com versão logica (operador! de negação ) portanto, se não esta vazio, mostre os interesses -->
+                <?php if (!empty($interesses)) { ?>
+                    <li>Interesses usando <code>implode()</code>:
+                        <!--transformando o array em string-->
+                        <?= implode(",", $interesses) ?>
+                    </li>
+                    <li>interesses - usando <code>foreach</code>:
+                        <ul>
+                            <?php foreach ($interesses as $interesse) { ?>
+                                <li><?= $interesse ?></li>
+                            <?php } ?>
+
+                        </ul>
+
+                    </li>
+                <?php } ?>
+                <li>Mensagem: <?= $mensagem ?></li>
+            </ul>
+        <?php    } ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
